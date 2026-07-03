2f80b26c4df3a38f97871b2b08d473a421fda025
trinhtrung20003-ux <trinhtrung20003@gmail.com>
2026-07-03 16:12:08 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,10 +11,10 @@ async def gui_tin():
         await bot.send_photo(
             chat_id=CHAT_ID,
             photo=photo,
-            caption="caption="""📌 Đang back mã Shopee 100K/0đ SVIP
+            caption="""📌 Đang back mã Shopee 100K/0đ SVIP
 
 Lưu tại:
-https://s.shopee.vn/20tWm6nM5Q""""
+https://s.shopee.vn/20tWm6nM5Q"""
         )
 
 asyncio.run(gui_tin())
