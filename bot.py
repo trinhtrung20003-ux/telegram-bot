1a60194a538e4e7d530f5e5d26797929b3b5b593
trinhtrung20003-ux <trinhtrung20003@gmail.com>
2026-07-03 15:59:22 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,34 +1,20 @@
 from telegram import Bot
 import asyncio
-import schedule
-import time
 
 TOKEN = "8026861100:AAEoy8Si4uVkud-OmVPU9uWNYH_hcIj80EM"
 CHAT_ID = "6086572676"
+
 bot = Bot(token=TOKEN)
+
 async def gui_tin():
-  with open("anh.jpg.jfif", "rb") as photo:
+    with open("anh.jpg.jfif", "rb") as photo:
         await bot.send_photo(
-            chat_id=6086572676,
+            chat_id=CHAT_ID,
             photo=photo,
-            caption="""📌 Đang back mã Shopee 100K/0đ SVIP
+            caption="caption="""📌 Đang back mã Shopee 100K/0đ SVIP
 
 Lưu tại:
-https://s.shopee.vn/20tWm6nM5Q"""
+https://s.shopee.vn/20tWm6nM5Q""""
         )
 
-
-def job():
-    asyncio.run(gui_tin())
-
-# Các giờ gửi
-schedule.every().day.at("00:01").do(job)
-schedule.every().day.at("09:00").do(job)
-schedule.every().day.at("12:00").do(job)
-schedule.every().day.at("20:00").do(job)
-
-print("Bot đang chạy...")
-
-while True:
-       schedule.run_pending()
-       time.sleep(1)
+asyncio.run(gui_tin())
