35be10b32c4aa95c3729171791bd89c15855a135
trinhtrung20003-ux <trinhtrung20003@gmail.com>
2026-07-05 12:14:00 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,14 @@ async def gui_tin():
         await bot.send_photo(
             chat_id=CHAT_ID,
             photo=photo,
-            caption="""📌 Đang back mã Shopee 100K/0đ SVIP
+            caption="""📌 Đang back lượt mã SVIP 40% - Giảm tối đa 100K/0đ dùng ngay
+
+► Lưu tại: https://s.shopee.vn/2qSjEc49TM
+
+⚠ Lưu ý:
+► Đối với ai đã lưu mã trước đó, sau khi thấy thông báo back bấm mở liên tục mục Shopee Voucher cho tới khi thấy mã 40% hiện, chọn và đặt hàng nhanh.
+
+► Đối với ai chưa lưu, canh trước khung giờ back mã hoặc sau khi có thông báo back mã thì load lại liên tục từ 0–2 phút. Sau khi lưu được thì vào mua nhanh. Nếu hết mã thì chờ lần back sau và làm theo cách đã lưu trước đó.
 
 Lưu tại:
 https://s.shopee.vn/20tWm6nM5Q"""
